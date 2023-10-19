7acbc1b2ccc6f063975f94517768ca1d0dac5759
AndyTRY <andyren.yuen@mail.utoronto.ca>
2023-10-19 17:30:31 -0400

    DataSyncService Inital Work Co-authored-by: Nazmus Saqeeb <namoosir@users.noreply.github.com>

--- Services/DataSynchronizationService/DataSynchronizationService.cs
+++ Services/DataSynchronizationService/DataSynchronizationService.cs
@@ -2,29 +2,76 @@ using MediaTrackerYoutubeService.Data;
 using MediaTrackerYoutubeService.Utils;
 using Microsoft.EntityFrameworkCore;
 using MediaTrackerYoutubeService.Utils.Youtube;
+using MediaTrackerYoutubeService.Services.AuthTokenExchangeService;
+using MediaTrackerYoutubeService.Models;
+using MediaTrackerYoutubeService.Services.UserService;
 
 namespace MediaTrackerYoutubeService.Services.DataSynchronizationService;
 
 public class DataSynchronizationService : IDataSynchronizationService
 {
     private readonly IDbContextFactory<AppDbContext> _context;
-    public DataSynchronizationService(IDbContextFactory<AppDbContext> context)
+    private readonly IAuthTokenExchangeService _authTokenExchangeService;
+    private readonly IConfiguration _configuration;
+    private readonly IUserService _userService;
+    
+    public DataSynchronizationService(IDbContextFactory<AppDbContext> context, IAuthTokenExchangeService authTokenExchangeService, IUserService userService, IConfiguration configuration)
     {
         _context = context;
+        _authTokenExchangeService = authTokenExchangeService;
+        _userService = userService;
+        _configuration = configuration;
     }
 
-    public void SyncData(int userId){
-        // string access_token = FetchAccessTokenFromAuth(userId);
-        // YoutubeAPIClient client = new YoutubeAPIClient(access_token, "SDFKJDSHNJK");
-        // SyncPlaylistsAndAssociatedVideos();
-        // SyncSubscriptionsAndAssociatedChannels();
+    public async void SyncData(int userId){
+        try 
+        {
+            if (!(await _userService.UpsertUser(userId)).Success) throw new Exception("Failed to add new user or confirm exisiting user");
+            
+            var tokenResult = await _authTokenExchangeService.YoutubeAuthTokenExchange(userId);
+            
+            if (!tokenResult.Success) throw new Exception("Failed to get youtube token");
+            
+            var access_token = tokenResult.Data;
+            var apiKey = _configuration["YoutubeAPIKey"] ?? throw new Exception("Missing ApiKey in Config");
+            
+            YoutubeAPIClient client = new YoutubeAPIClient(access_token, apiKey);
+            
+            
+            await Task.WhenAll(SyncPlaylistsAndAssociatedVideos(userId, client),
+                                SyncSubscriptionsAndAssociatedChannels(userId, client));
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine(e);
+        }
     }
 
-    private void SyncPlaylistsAndAssociatedVideos(YoutubeAPIClient client){
+    private async Task SyncPlaylistsAndAssociatedVideos(int userId, YoutubeAPIClient client){
+        try {
+            var getUserResponse = await _userService.GetUser(userId);
+            if (!getUserResponse.Success) throw new Exception("Failed to get User");
+            var playlistsResponse = await client.GetMyPlaylists();
 
+
+            // playlistsInternal.
+            
+
+
+            var playlistsExternal = playlistsResponse.items;
+            var playlistsInternal = getUserResponse.Data!.VideoPlaylists;
+
+            // foreach (var playlist in playlists) {
+                
+            // }
+
+
+        } catch {
+
+        }
     }
 
-    private void SyncSubscriptionsAndAssociatedChannels(YoutubeAPIClient client){
-        
+    private async Task SyncSubscriptionsAndAssociatedChannels(int userId, YoutubeAPIClient client){
+        throw new NotImplementedException();
     }
 }
