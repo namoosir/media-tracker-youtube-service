5be102b9149233932d1e9b9e53954cb3af17e8a6
AndyTRY <andyren.yuen@mail.utoronto.ca>
2023-11-08 18:40:15 -0500

    Work on FetchService
    Co-authored-by: Nazmus Saqeeb <namoosir@users.noreply.github.com>

--- Services/FetchYoutubeDataService/FetchYoutubeDataService.cs
+++ Services/FetchYoutubeDataService/FetchYoutubeDataService.cs
@@ -1,19 +1,89 @@
 using MediaTrackerYoutubeService.Models;
 using MediaTrackerYoutubeService.Schemas;
+using MediaTrackerYoutubeService.Utils.Youtube;
 
 namespace MediaTrackerYoutubeService.Services.FetchYoutubeDataService;
 
 public class FetchYoutubeDataService : IFetchYoutubeDataService
 {
-    private readonly HttpClient _httpClient;
+    public FetchYoutubeDataService() { }
 
-    public FetchYoutubeDataService(HttpClient httpClient)
+    public async Task<ServiceResponse<List<Resource>>> fetchExternalPlaylists(
+        YoutubeAPIClient client
+    )
     {
-        _httpClient = httpClient;
+        try
+        {
+            List<Resource> playlistsExternal = new();
+            var playlistsResponse = await client.GetMyPlaylists();
+
+            playlistsExternal.AddRange(playlistsResponse.items);
+
+            while (playlistsResponse.nextPageToken != null)
+            {
+                playlistsResponse = await client.GetMyPlaylists(playlistsResponse.nextPageToken);
+                playlistsExternal.AddRange(playlistsResponse.items);
+            }
+
+            return ServiceResponse<List<Resource>>.Build(playlistsExternal, true, null);
+        }
+        catch (Exception e)
+        {
+            return ServiceResponse<List<Resource>>.Build(null, false, e.ToString());
+        }
     }
 
-    // public async Task<ServiceResponse<List<UserYoutubeVideo>>> FetchLikedVideos(UserInformation userInformation)
-    // {
-    //     throw new NotImplementedException();
-    // }
+    public async Task<ServiceResponse<List<Resource>>> fetchExternalPlaylistVideos(
+        YoutubeAPIClient client,
+        string playlistId
+    )
+    {
+        try
+        {
+            List<Resource> playlistVideosExternal = new();
+            var playlistItemsResponse = await client.GetMyPlaylistItems(playlistId);
+
+            playlistVideosExternal.AddRange(playlistItemsResponse.items);
+
+            while (playlistItemsResponse.nextPageToken != null)
+            {
+                playlistItemsResponse = await client.GetMyPlaylistItems(
+                    playlistId,
+                    playlistItemsResponse.nextPageToken
+                );
+                playlistVideosExternal.AddRange(playlistItemsResponse.items);
+            }
+
+            return ServiceResponse<List<Resource>>.Build(playlistVideosExternal, true, null);
+        }
+        catch (Exception e)
+        {
+            return ServiceResponse<List<Resource>>.Build(null, false, e.ToString());
+        }
+    }
+
+    public async Task<ServiceResponse<List<Resource>>> fetchChannels(
+        YoutubeAPIClient client,
+        List<Channel> channelsToUpdate
+    )
+    {
+        List<string> channelIdsToUpdate = channelsToUpdate
+            .Select(channel => channel.YoutubeId)
+            .ToList();
+
+        int blockSize = 50;
+        List<List<string>> channelIdsToUpdateBlocks = Enumerable
+            .Range(0, (channelIdsToUpdate.Count + blockSize - 1) / blockSize)
+            .Select(i => channelIdsToUpdate.Skip(i * blockSize).Take(blockSize).ToList())
+            .ToList();
+
+        List<Resource> channelsToUpdateResource = new List<Resource>();
+        foreach (List<string> channelIdsBlock in channelIdsToUpdateBlocks)
+        {
+            var getChannelsResponse = await client.GetChannels(channelIdsBlock);
+            channelsToUpdateResource.AddRange(getChannelsResponse.items);
+        }
+
+        return ServiceResponse<List<Resource>>.Build(channelsToUpdateResource, true, null);
+    }
 }
--- Services/FetchYoutubeDataService/IFetchYoutubeDataService.cs
+++ Services/FetchYoutubeDataService/IFetchYoutubeDataService.cs
@@ -1,9 +1,19 @@
 using MediaTrackerYoutubeService.Models;
 using MediaTrackerYoutubeService.Schemas;
+using MediaTrackerYoutubeService.Utils.Youtube;
 
 namespace MediaTrackerYoutubeService.Services.FetchYoutubeDataService;
 
 public interface IFetchYoutubeDataService
 {
-    // Task<ServiceResponse<List<UserYoutubeVideo>>> FetchLikedVideos(UserInformation userInformation);
+    Task<ServiceResponse<List<Resource>>> fetchExternalPlaylists(YoutubeAPIClient client);
+    Task<ServiceResponse<List<Resource>>> fetchExternalPlaylistVideos(
+        YoutubeAPIClient client,
+        string playlistId
+    );
+
+    Task<ServiceResponse<List<Resource>>> fetchChannels(
+        YoutubeAPIClient client,
+        List<Channel> channelsToUpdate
+    );
 }
